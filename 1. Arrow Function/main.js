b91a4e1b7118115f08fb7fe9841b5371e3c07c29
Kushal Shrestha <104309319+KushalxStha@users.noreply.github.com>
2023-10-02 22:24:22 +0545

    Update main.js

--- 1. Arrow Function/main.js
+++ 1. Arrow Function/main.js
@@ -1,1 +1,14 @@
+let hello = "";
 
+// Before arrow
+hello = function() {
+  return "Hello World!";
+}
+
+// With arrow function
+hello = () => {
+  return "Hello World!";
+}
+
+// Arrow function returns value by default
+hello = () => "Hello World!";
