3c51bca5ec78f0c42f24e1fb11449c4c6ac932d0
Kushal Shrestha <shresthakushal61@gmail.com>
2024-04-09 12:55:25 +0545

    Destructuring Object

---
+++ 07-destructuring-object/main.js
@@ -0,0 +1,12 @@
+let user = {
+  name: "Kushal",
+  address: "Maitidevi",
+  college: "ASCOL"
+};
+
+let { name: n, address, college } = user;
+// variable should be same as attributes of obj.
+
+console.log(n);
+console.log(address);
+console.log(college);
